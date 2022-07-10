8d69563633547a7629572b8c5608d4786141dde5
David Boreham <david@bozemanpass.com>
2022-07-09 21:53:04 -0600

    Use channels

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -2,11 +2,17 @@ package orchestrator
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/dboreham/db-locking-test/sessions"
 )
 
 func Run() {
 	fmt.Println("Orchestrator run called")
-	sessions.Run()
+	advance := make(chan int)
+	phase := make(chan int)
+	sessions.Start(advance, phase)
+	advance <- 1
+	what := <-phase
+	log.Println(what)
 }
--- sessions/session1.go
+++ sessions/session1.go
@@ -8,8 +8,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func Run() {
-	connectionString := fmt.Sprintf("postgresql://db:db@localhost:49153/db?sslmode=disable")
+func run() {
+	connectionString := fmt.Sprintf("postgresql://db:db@localhost:57381/db?sslmode=disable")
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatalln(err)
@@ -23,4 +23,22 @@ func Run() {
 		rows.Scan(&row)
 		fmt.Println(row)
 	}
+}
+
+// Call this to start the session
+// Session waits before advancing to execute the next phase
+// upon recipt of a message on its advance channel
+// Session sends a message on its phase channel to indicate it has completed the phase
+// (and is waiting)
+func Start(advance <-chan int, phase chan<- int) {
+	go func() {
+		// Wait on a message on the advance channel
+		x := <-advance
+		log.Println(x)
+		// Execute a sleep query
+		log.Println("Executing query")
+		run()
+		// Signal to the orchestrator that we've completed the query
+		phase <- 2
+	}()
 }
